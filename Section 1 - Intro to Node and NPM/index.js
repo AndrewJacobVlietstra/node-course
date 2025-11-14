fdac42c1a9c05a625b5e6eee854bc0cd60960a70
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-11-14 14:03:26 -0500

    Wrote some blocking and non-blocking code to read and write files using callback functions.

--- Section 1 - Intro to Node and NPM/index.js
+++ Section 1 - Intro to Node and NPM/index.js
@@ -1,7 +1,28 @@
-const fs = require('fs');
+const fs = require("fs");
+const date = new Date();
 
-const textInput = fs.readFileSync('./text.txt', 'utf8');
-console.log(textInput);
+// Synchronous blocking code execution
+// const textInput = fs.readFileSync("./text.txt", "utf-8");
+// console.log(textInput);
 
-const textOutput = `This is an example text output: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./output.txt', textOutput);
+// const textOutput = `This is an example of text output: ${textInput}\nCreated on ${date.toLocaleDateString()}`;
+// fs.writeFileSync("./output.txt", textOutput);
+// console.log("File written!");
+
+// Asynchronous non-blocking code
+fs.readFile("./start.txt", "utf-8", (err, data1) => {
+	if (err) return console.log("Error ocurred!");
+
+	fs.readFile(`./${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
+		fs.readFile("./append.txt", "utf-8", (err, data3) => {
+			console.log(data3);
+
+			fs.writeFile("./final.txt", `${data2}\n${data3}`, (err) => {
+				console.log("File has been written!");
+			});
+		});
+	});
+});
+
+console.log("Reading file...");
