c802b7c7707347fc86a07bf92ddfcadc43ce04ef
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-11-15 17:23:13 -0500

    Wrote simple routes in node server and have fallback page not found 404 error with html headers if route not matched.

--- Section 1 - Intro to Node and NPM/index.js
+++ Section 1 - Intro to Node and NPM/index.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
 const date = new Date();
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////////////////////
+// FILES
 
 // Synchronous blocking code execution
 // const textInput = fs.readFileSync("./text.txt", "utf-8");
@@ -10,19 +15,44 @@ const date = new Date();
 // console.log("File written!");
 
 // Asynchronous non-blocking code
-fs.readFile("./start.txt", "utf-8", (err, data1) => {
-	if (err) return console.log("Error ocurred!");
+// fs.readFile("./start.txt", "utf-8", (err, data1) => {
+// 	if (err) return console.log("Error ocurred!");
 
-	fs.readFile(`./${data1}.txt`, "utf-8", (err, data2) => {
-		console.log(data2);
-		fs.readFile("./append.txt", "utf-8", (err, data3) => {
-			console.log(data3);
+// 	fs.readFile(`./${data1}.txt`, "utf-8", (err, data2) => {
+// 		console.log(data2);
+// 		fs.readFile("./append.txt", "utf-8", (err, data3) => {
+// 			console.log(data3);
 
-			fs.writeFile("./final.txt", `${data2}\n${data3}`, (err) => {
-				console.log("File has been written!");
-			});
-		});
+// 			fs.writeFile("./final.txt", `${data2}\n${data3}`, (err) => {
+// 				console.log("File has been written!");
+// 			});
+// 		});
+// 	});
+// });
+
+// console.log("Reading file...");
+
+//////////////////////////////////////////
+// SERVER
+const portName = 8000;
+
+const server = http.createServer((req, res) => {
+	const pathname = req.url;
+
+	if (pathname === "/" || pathname === "/overview") {
+		return res.end("This is the Overview!");
+	}
+	if (pathname === "/product") {
+		return res.end("This is the Product!");
+	}
+
+	res.writeHead(404, {
+		"Content-type": "text/html",
+		"My-Own-Header": "Hello World",
 	});
+	res.end("<h1>Page Not Found!</h1>");
 });
 
-console.log("Reading file...");
+server.listen(portName, "127.0.0.1", () => {
+	console.log(`Listening to requests on port ${portName}...`);
+});
